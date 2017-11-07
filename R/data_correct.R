8df9f9322225ea2ee6626426cdeab3eccf9d42aa
dgohel <david.gohel@ardata.fr>
2017-11-07 16:49:29 +0100

    drop ctry condition

--- R/data_correct.R
+++ R/data_correct.R
@@ -26,15 +26,30 @@ data_correct_with_rules <- function( data ){
 
 }
 
+when_false_str <- function(rules){
+  str1 <- map_chr(rules, function(x) paste0( "(", paste0( "!", x$when_false, collapse = " & " ), ")") )
+  str1
+}
+when_true_str <- function(rules){
+  str1 <- map_chr(rules, function(x) paste0( "(", paste( x$when_true, collapse = " & " ), ")") )
+  str1
+}
+countries_str <- function(rules){
+  str1 <- map_chr(rules, function(x) {
+    if( length(x$country) < 1 ) "TRUE"
+    else paste0( "( country %in% c(", paste( shQuote(x$country), collapse = ", " ), ") )" )
+  } )
+  str1
+}
 
 #' @importFrom purrr map_chr
 correct_exprs <- function( correct_rules ){
 
   rules <- yaml::yaml.load_file(correct_rules)
 
-  str1 <- map_chr(rules, function(x) paste0( "(", paste0( "!", x$when_false, collapse = " & " ), ")") )
-  str2 <- map_chr(rules, function(x) paste0( "(", paste( x$when_true, collapse = " & " ), ")") )
-  str3 <- map_chr(rules, function(x) paste0( "( country %in% c(", paste( shQuote(x$country), collapse = ", " ), ") )" ) )
+  str1 <- when_false_str(rules)
+  str2 <- when_true_str(rules)
+  str3 <- countries_str(rules)
   cond_ <- sprintf( "%s & %s & %s", str1, str2, str3)
 
   replace_var <- map_chr(rules, function(x) x$replace )
@@ -50,9 +65,9 @@ mark_correct_exprs <- function( correct_rules ){
 
   rules <- yaml::yaml.load_file(correct_rules)
 
-  str1 <- map_chr(rules, function(x) paste0( "(", paste0( "!", x$when_false, collapse = " & " ), ")") )
-  str2 <- map_chr(rules, function(x) paste0( "(", paste( x$when_true, collapse = " & " ), ")") )
-  str3 <- map_chr(rules, function(x) paste0( "( country %in% c(", paste( shQuote(x$country), collapse = ", " ), ") )" ) )
+  str1 <- when_false_str(rules)
+  str2 <- when_true_str(rules)
+  str3 <- countries_str(rules)
   cond_ <- sprintf( "%s & %s & %s", str1, str2, str3)
 
   replace_var <- rule_names(rules)
