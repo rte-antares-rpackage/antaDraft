2114a22ac4e42fb61dbf8a0fe4ced133342e5fd3
dgohel <david.gohel@ardata.fr>
2017-11-21 01:55:31 +0100

    add test

--- tests/testthat/test-load-read.R
+++ tests/testthat/test-load-read.R
@@ -1,2 +1,21 @@
 context("load reading")
 
+test_that("expected columns", {
+  load_dir <- system.file(package = "antaDraft", "data_sample")
+  load_data <- anta_load_read(data_dir = load_dir )
+
+  expect_true( all( c("MapCode", "AreaTypeCode", "DateTime", "AreaName", "TotalLoadValue",
+    "country", "observed") %in% names(load_data) ) )
+
+  expect_is(load_data$DateTime, "POSIXct")
+  expect_is(load_data$TotalLoadValue, "numeric")
+
+})
+
+test_that("missing combinations are added", {
+  load_dir <- system.file(package = "antaDraft", "data_sample")
+  load_data <- anta_load_read(data_dir = load_dir )
+
+  likely_not_missing <- !is.na( load_data$TotalLoadValue ) & !is.na( load_data$MapCode )
+  expect_equal(nrow(load_data[likely_not_missing,]), sum(load_data$observed, na.rm = TRUE))
+})
