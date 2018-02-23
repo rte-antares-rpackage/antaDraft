94d0cbba4401d736b0fed14ba3ad57454554a026
dgohel <david.gohel@ardata.fr>
2018-02-23 10:42:36 +0100

    init_antares_project

---
+++ R/init_antares_project.R
@@ -0,0 +1,7 @@
+#' @importFrom utils unzip
+#' @export
+init_antares_project <- function(path){
+  f_template <- system.file(package = package_name, "etude_vide_package.zip")
+
+  unzip(zipfile = f_template, exdir = path )
+}
