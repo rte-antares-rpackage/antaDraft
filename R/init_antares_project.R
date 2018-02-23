0357f96a941084bd7126485465fdde8b2fe2876c
dgohel <david.gohel@ardata.fr>
2018-02-23 10:44:46 +0100

    init_antares_project

--- R/init_antares_project.R
+++ R/init_antares_project.R
@@ -1,5 +1,11 @@
 #' @importFrom utils unzip
 #' @export
+#' @title create an empty antares project
+#' @description create an empty antares project to be then
+#' enriched with data.
+#' @param path folder to be created
+#' @examples
+#' init_antares_project("demo_proj")
 init_antares_project <- function(path){
   f_template <- system.file(package = package_name, "etude_vide_package.zip")
 
